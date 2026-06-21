f5769fe65da3837d398272612c1ade047fccd3ed
Adeline2222 <adelineshkila0@gmail.com>
2026-06-21 17:25:27 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,6 @@ use App\Http\Controllers\AnggotaController;
 use App\Http\Controllers\BukuController;
 use App\Http\Controllers\PeminjamanController;
 
-Route::get('/', function () {
-    return redirect('/login');
-});
 /*
 | LOGIN
 */
